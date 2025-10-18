0208418b13899cbddc67c3b12a06f20bb9e0528c
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2025-10-18 17:12:28 +0300

    Added an pseudo rb listing with roles controler model and view

---
+++ AndreiWeb/Controllers/RolesController.cs
@@ -0,0 +1,21 @@
+﻿using AndreiWeb.Data;
+using AndreiWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AndreiWeb.Controllers;
+
+public class RolesController : Controller
+{
+    // GET
+    private readonly ApplicationDbContext _db;
+    public RolesController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult ListRoles()
+    {
+        var objRolesListAllNotAdmin = _db.Roles.Where(role => role.Name != RoleName.Admin)
+                                        .ToList();
+        return View(objRolesListAllNotAdmin);
+    }
+}
--- AndreiWeb/Data/ApplicationDbContext.cs
+++ AndreiWeb/Data/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ public class ApplicationDbContext: DbContext
     }
 
     public DbSet<Category> Categories { get; set; }
+    public DbSet<Roles> Roles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -20,5 +21,13 @@ public class ApplicationDbContext: DbContext
          new Category{Id = 2, Name = "SciFi", DisplayOrder = 2},
          new Category{Id = 3, Name = "History", DisplayOrder = 3}
             );
+        modelBuilder.Entity<Roles>()
+            .Property(r => r.Name)
+            .HasConversion<string>();
+        modelBuilder.Entity<Roles>().HasData(
+            new Roles { Id = 1, Name = RoleName.Admin},
+            new Roles { Id = 2, Name = RoleName.Customer},
+            new Roles { Id = 3, Name = RoleName.User}
+        );
     }
 }
