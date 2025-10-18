79e348f04659350eb4be3d8b156657eef8bff3bc
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2025-10-18 03:38:03 +0300

    added db migration and dbset for category

---
+++ AndreiWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AndreiWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure.Internal;
+
+namespace AndreiWeb.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+    
+    }
+
+    public DbSet<Category> Categories { get; set; }
+}
