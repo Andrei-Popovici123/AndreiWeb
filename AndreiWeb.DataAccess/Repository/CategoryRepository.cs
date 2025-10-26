9f28f2305718b219b86c7e9398feca206843e66e
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2025-10-27 01:05:09 +0200

    Added Repository For Category

---
+++ AndreiWeb.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using AndreiWeb.DataAccess.Data;
+using AndreiWeb.DataAccess.Repository.IRepository;
+using AndreiWeb.Models;
+
+namespace AndreiWeb.DataAccess.Repository;
+
+public class CategoryRepository :  Repository<Category>, ICategoryRepository
+{
+    private ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = _db;
+    }
+
+    public void Update(Category category)
+    {
+        _db.Categories.Update(category);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+        
+    }
+}
