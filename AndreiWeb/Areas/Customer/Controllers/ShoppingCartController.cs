b8b18e141ee0236dca30c386bc6a5d3f8fa933f2
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2025-10-30 15:54:41 +0200

    Added Shopping Cart View  and controller

---
+++ AndreiWeb/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AndreiWeb.Areas.Customer.Controllers;
+[Area("Customer")]
+public class ShoppingCartController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
