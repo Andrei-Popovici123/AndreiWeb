18bd3e1d835d3c567f8683923248167346463fe8
Popovici Andrei-Cristian <andrei-cristian.popovici@student.upt.ro>
2025-10-30 02:12:53 +0200

    Company Contorller

--- AndreiWeb/Areas/Admin/Controllers/CompanyController.cs
+++ AndreiWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -15,91 +15,52 @@ namespace AndreiWeb.Areas.Admin.Controllers;
 public class CompanyController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IWebHostEnvironment _webHostEnvironment;
 
     public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
     {
         _unitOfWork = unitOfWork;
-        _webHostEnvironment = webHostEnvironment;
     }
 
     public IActionResult Index()
     {
-        return View(_unitOfWork.Product.GetAll(includeProperties: "Category").ToList());
+        return View(_unitOfWork.Company.GetAll().ToList());
     }
 
     public IActionResult Upsert(int? id)
     {
-        // Create op is being processed with View Model Consisting of Product and Category List
-        ProductViewModel productViewModel = new()
-        {
-            CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-            {
-                Text = u.Name,
-                Value = u.Id.ToString(),
-            }),
-            Product = new Product(),
-        };
+
         if (id == null || id == 0)
         {
-            return View(productViewModel);
+            return View(new Company());
         }
         else
         {
-            productViewModel.Product = _unitOfWork.Product.Get(u => u.Id == id);
-            return View(productViewModel);
+            Company company = _unitOfWork.Company.Get(u => u.Id == id);
+            return View(company);
         }
     }
 
     [HttpPost]
-    public IActionResult Upsert(ProductViewModel productViewModel, IFormFile? file)
+    public IActionResult Upsert(Company company)
     {
         if (ModelState.IsValid)
         {
-            string wwwRootPath = _webHostEnvironment.WebRootPath;
-            if (file != null)
+
+            if (company.Id == 0)
             {
-                string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-                string productPath = Path.Combine(wwwRootPath, @"images\product");
-                if (!string.IsNullOrEmpty(productViewModel.Product.ImageUrl))
-                {
-                    //delete the old image
-                    var oldImagePath = Path.Combine(wwwRootPath, productViewModel.Product.ImageUrl.TrimStart('\\'));
-                    if (System.IO.File.Exists(oldImagePath))
-                    {
-                        System.IO.File.Delete(oldImagePath);
-                    }
-                }
-
-                using (var fileStream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
-                {
-                    file.CopyTo(fileStream);
-                }
-
-                productViewModel.Product.ImageUrl = @"\images\product\" + filename;
-            }
-
-            if (productViewModel.Product.Id == 0)
-            {
-                _unitOfWork.Product.Add(productViewModel.Product);
+                _unitOfWork.Company.Add(company);
             }
             else
             {
-                _unitOfWork.Product.Update(productViewModel.Product);
+                _unitOfWork.Company.Update(company);
             }
 
             _unitOfWork.Save();
-            TempData["success"] = "New Product Added Succesfully";
-            return RedirectToAction("Index", "Product");
+            TempData["success"] = "New Company Added Successfully";
+            return RedirectToAction("Index", "Company");
         }
-
-        productViewModel.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-        {
-            Text = u.Name,
-            Value = u.Id.ToString(),
-        });
-
-        return View(productViewModel);
+        
+        return View(company);
     }
 
     #region API CALLS
@@ -107,26 +68,19 @@ public class CompanyController : Controller
     [HttpGet]
     public IActionResult GetAll()
     {
-        List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
-        return Json(new { data = objProductList });
+        List<Company> objCompanyList = _unitOfWork.Company.GetAll().ToList();
+        return Json(new { data = objCompanyList });
     }
 
     [HttpDelete]
     public IActionResult Delete(int? id)
     {
-        var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
-        if (productToBeDeleted == null)
+        var companyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+        if (companyToBeDeleted == null)
         {
             return Json(new { success = false, message = "Error while deleting" });
         }
-
-        var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
-        if (System.IO.File.Exists(oldImagePath))
-        {
-            System.IO.File.Delete(oldImagePath);
-        }
-
-        _unitOfWork.Product.Remove(productToBeDeleted);
+        _unitOfWork.Company.Remove(companyToBeDeleted);
         _unitOfWork.Save();
         return Json(new { success = true, message = "Delete Successful" });
     }
